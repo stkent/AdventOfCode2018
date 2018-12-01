74653519168dae21146c605bd26acf6c45ec6c27
Stuart Kent <skent.dev@gmail.com>
2018-12-01 17:08:14 -0600

    Add firstRepeatIndex variants

--- utilities/src/main/kotlin/extensions/Iterator.kt
+++ utilities/src/main/kotlin/extensions/Iterator.kt
@@ -31,3 +31,31 @@ fun <T> Iterator<T>.firstRepeat(targetCount: Int): T? {
 
     return null
 }
+
+fun <T> Iterator<T>.firstRepeatIndex(): Int? {
+    val seen = mutableSetOf<T>()
+
+    for (element in withIndex()) {
+        if (!seen.add(element.value)) return element.index
+    }
+
+    return null
+}
+
+fun <T> Iterator<T>.firstRepeatIndex(targetCount: Int): Int? {
+    check(targetCount >= 3) {
+        "This method cannot be called with a targetCount less than 3."
+    }
+
+    val seen = HashMultiset.create<T>()
+
+    for (element in withIndex()) {
+        if (seen.count(element.value) + 1 == targetCount) {
+            return element.index
+        }
+
+        seen.add(element.value)
+    }
+
+    return null
+}
