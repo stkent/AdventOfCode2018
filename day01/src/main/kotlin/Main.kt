fae3d392e0e8cefb02acd2c8dbc8a439ab467866
Stuart Kent <skent.dev@gmail.com>
2018-12-01 00:17:28 -0500

    day01: initial solution

--- day01/src/main/kotlin/Main.kt
+++ day01/src/main/kotlin/Main.kt
@@ -1,3 +1,25 @@
 fun main() {
     val input = resourceFile("input.txt")
+
+//    println(input.readLines()
+//        .map { Integer.parseInt(it) }
+//        .sum())
+
+    var acc = 0
+    val seen = mutableSetOf(0)
+
+    val allLines = input.readLines()
+    var index = 0
+
+    while (true) {
+        acc += Integer.parseInt(allLines[index])
+
+        if (seen.contains(acc)) {
+            println(acc)
+            break
+        }
+
+        seen.add(acc)
+        index = (++index).rem(allLines.size)
+    }
 }
