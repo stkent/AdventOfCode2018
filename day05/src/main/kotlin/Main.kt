399514a669b97a9205178ef30ad79df59198f6a8
Stuart Kent <skent.dev@gmail.com>
2018-12-05 12:25:23 -0500

    day05: initial solution

--- day05/src/main/kotlin/Main.kt
+++ day05/src/main/kotlin/Main.kt
@@ -1,3 +1,38 @@
 fun main() {
-    val input = resourceFile("input.txt")
+    val input = resourceFile("input.txt").readLines().first()
+
+
+
+    println(fullyReact(input).length)
+
+    val l = ('a'..'z')
+        .map { char -> "$char|${char.toUpperCase()}".toRegex() }
+        .map { regex -> input.replace(regex, "") }
+        .map { s -> fullyReact(s) }
+        .minBy { it.length }
+
+    println(l!!.length)
+
+//    var output = input
+//
+//    while (output.contains())
+}
+
+fun fullyReact(s: String): String {
+    val replacementRegex = ('a'..'z')
+        .flatMap { char ->
+            val upperChar = char.toUpperCase()
+            return@flatMap listOf("$char$upperChar", "$upperChar$char")
+        }
+        .joinToString(separator = "|").toRegex()
+
+    var result = s
+
+    while (true) {
+        val updatedResult = result.replace(replacementRegex, "")
+        if (updatedResult == result) break
+        result = updatedResult
+    }
+
+    return result
 }
