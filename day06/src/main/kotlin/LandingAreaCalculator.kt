ced1b206e84629505e3276163d0fe15cd1386a83
Stuart Kent <skent.dev@gmail.com>
2018-12-07 20:25:52 -0500

    day06: cleanup

---
+++ day06/src/main/kotlin/LandingAreaCalculator.kt
@@ -0,0 +1,59 @@
+import extensions.extractInts
+
+class LandingAreaCalculator(rawTargetPoints: List<String>) {
+
+    private val targetPoints = rawTargetPoints.map { string ->
+        string.extractInts().run { GridPoint2d(x = first(), y = last()) }
+    }
+
+    private val targetXs = targetPoints.map(GridPoint2d::x)
+    private val targetYs = targetPoints.map(GridPoint2d::y)
+    private val minTargetX = targetXs.min()!!
+    private val maxTargetX = targetXs.max()!!
+    private val minTargetY = targetYs.min()!!
+    private val maxTargetY = targetYs.max()!!
+
+    private val nearbyPoints = run {
+        val result = mutableListOf<GridPoint2d>()
+
+        for (x in minTargetX..maxTargetX) {
+            for (y in minTargetY..maxTargetY) {
+                result.add(GridPoint2d(x, y))
+            }
+        }
+
+        return@run result
+    }
+
+    val safeArea1: Int by lazy {
+        nearbyPoints
+            .groupBy { nearbyPoint -> targetPoints.uniquePointClosestTo(nearbyPoint) }
+            .filterValues { targetRegion ->
+                targetRegion.isWithinBounds(minTargetX + 1 until maxTargetX, minTargetY + 1 until maxTargetY)
+            }
+            .map { (_, targetRegionPoints) -> targetRegionPoints.count() }
+            .max()!!
+    }
+
+    val safeArea2: Int by lazy {
+        nearbyPoints.count { nearbyPoint ->
+            targetPoints.sumBy { it.l1DistanceTo(nearbyPoint) } < 10000
+        }
+    }
+
+}
+
+private fun Collection<GridPoint2d>.uniquePointClosestTo(other: GridPoint2d): GridPoint2d? {
+    val pointDistances = this
+        .map { otherPoint -> otherPoint to otherPoint.l1DistanceTo(other) }
+        .sortedBy { (_, distance) -> distance }
+
+    val (closestPoint, shortestDistance) = pointDistances[0]
+    val (_, secondShortestDistance) = pointDistances[1]
+
+    return if (shortestDistance < secondShortestDistance) closestPoint else null
+}
+
+private fun Collection<GridPoint2d>.isWithinBounds(xBounds: IntRange, yBounds: IntRange): Boolean {
+    return this.all { point -> point.inBounds(xBounds, yBounds) }
+}
--- day06/src/main/kotlin/Main.kt
+++ day06/src/main/kotlin/Main.kt
@@ -1,57 +1,9 @@
-import extensions.extractInts
-
 fun main() {
     val input = resourceFile("input.txt").readLines()
 
-    val targetPoints = input.map { string ->
-        string.extractInts().run { GridPoint2d(x = first(), y = last()) }
-    }
+    val landingAreaCalculator = LandingAreaCalculator(rawTargetPoints = input)
 
-    val targetXs = targetPoints.map(GridPoint2d::x)
-    val targetYs = targetPoints.map(GridPoint2d::y)
-    val minTargetX = targetXs.min()!!
-    val maxTargetX = targetXs.max()!!
-    val minTargetY = targetYs.min()!!
-    val maxTargetY = targetYs.max()!!
-
-    val nearbyPoints = GridPoint2d.region(
-        minX = minTargetX,
-        maxX = maxTargetX,
-        minY = minTargetY,
-        maxY = maxTargetY)
-
-    val largestFiniteTargetRegion = nearbyPoints
-        .groupBy { nearbyPoint -> targetPoints.uniquePointClosestTo(nearbyPoint) }
-        .filterValues { targetRegion ->
-            targetRegion.isWithinBounds(minTargetX + 1 until maxTargetX, minTargetY + 1 until maxTargetY)
-        }
-        .map { (_, targetRegionPoints) -> targetRegionPoints.count() }
-        .max()!!
-
-    // part 1
-
-    println(largestFiniteTargetRegion)
-
-    // part 2
-
-    val regionSize = nearbyPoints.count { nearbyPoint ->
-        targetPoints.sumBy { it.l1DistanceTo(nearbyPoint) } < 10000
-    }
-
-    println(regionSize)
+    println("Part 1 solution: ${landingAreaCalculator.safeArea1}")
+    println("Part 2 solution: ${landingAreaCalculator.safeArea2}")
 }
 
-fun Collection<GridPoint2d>.uniquePointClosestTo(other: GridPoint2d): GridPoint2d? {
-    val pointDistances = this
-        .map { otherPoint -> otherPoint to otherPoint.l1DistanceTo(other) }
-        .sortedBy { (_, distance) -> distance }
-
-    val (closestPoint, shortestDistance) = pointDistances[0]
-    val (_, secondShortestDistance) = pointDistances[1]
-
-    return if (shortestDistance < secondShortestDistance) closestPoint else null
-}
-
-fun Collection<GridPoint2d>.isWithinBounds(xBounds: IntRange, yBounds: IntRange): Boolean {
-    return this.all { point -> point.inBounds(xBounds, yBounds) }
-}
